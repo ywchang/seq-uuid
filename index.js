12f610070661205636ab876cb336bcf84f6517cb
Yanwei <ywchang@thoughtworks.com>
2019-10-04 22:19:13 +0800

    [Yanwei] generate one seq byte when interval and length both 1 :watch:

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-exports.format = (bytes) => {
-  const part1 = Buffer.from(bytes.slice(0, 4)).toString('hex')
-  const part2 = Buffer.from(bytes.slice(4, 6)).toString('hex')
-  const part3 = Buffer.from(bytes.slice(6, 8)).toString('hex')
-  const part4 = Buffer.from(bytes.slice(8, 10)).toString('hex')
-  const part5 = Buffer.from(bytes.slice(10, 16)).toString('hex')
+exports.format = (byteList) => {
+  const part1 = Buffer.from(byteList.slice(0, 4)).toString('hex')
+  const part2 = Buffer.from(byteList.slice(4, 6)).toString('hex')
+  const part3 = Buffer.from(byteList.slice(6, 8)).toString('hex')
+  const part4 = Buffer.from(byteList.slice(8, 10)).toString('hex')
+  const part5 = Buffer.from(byteList.slice(10, 16)).toString('hex')
   return `${part1}-${part2}-${part3}-${part4}-${part5}`
+}
+
+exports.generate = (interval, length) => {
+  const base = Date.now()
+  const byteList = []
+  byteList.push(base & 0xff)
+  return [0xcd]
 }
--- test/index.test.js
+++ test/index.test.js
@@ -1,8 +1,20 @@
 const test = require('tape')
-const { format } = require('../')
+const sinon = require('sinon')
+const { format, generate } = require('../')
 
-test('should format from bytes to stardard uuid string', (t) => {
-  t.equal(format([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]),
-    '01020304-0506-0708-090a-0b0c0d0e0f10')
-  t.end()
+test('#format', t => {
+  t.test('should format from bytes to stardard uuid string', q => {
+    q.equal(format([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]),
+      '01020304-0506-0708-090a-0b0c0d0e0f10')
+    q.end()
+  })
+})
+
+test('#generate', t => {
+  t.test('should generate one seq byte if interval is 1 ms and length is 1 byte', q => {
+    sinon.stub(Date, 'now').returns(0xabcd)
+    q.deepEqual(generate(1, 1).slice(0, 1), [0xcd])
+    sinon.restore()
+    q.end()
+  })
 })
