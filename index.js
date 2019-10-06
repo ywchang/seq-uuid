714a7fea3de0782ce1b0be5ffd988ffd6cf46382
Yanwei <ywchang@thoughtworks.com>
2019-10-06 19:53:21 +0800

    [Yanwei] add random bytes to form complete uuid string :simple_smile:

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const getSeqBytes = (interval, length) => {
+const crypto = require('crypto')
+
+const getSeq = (interval, length) => {
   const seqBytes = []
   let base = Date.now() / interval
   for (let i = 0; i < length; i++) {
@@ -8,15 +10,31 @@ const getSeqBytes = (interval, length) => {
   return seqBytes.reverse()
 }
 
-exports.format = (byteList) => {
-  const part1 = Buffer.from(byteList.slice(0, 4)).toString('hex')
-  const part2 = Buffer.from(byteList.slice(4, 6)).toString('hex')
-  const part3 = Buffer.from(byteList.slice(6, 8)).toString('hex')
-  const part4 = Buffer.from(byteList.slice(8, 10)).toString('hex')
-  const part5 = Buffer.from(byteList.slice(10, 16)).toString('hex')
+const fillRandom = (byteStream) => {
+  const randomBytes = crypto.randomBytes(16)
+  for (let j = 0, i = byteStream.length; i < 16; i++) {
+    byteStream[i] = randomBytes[j++]
+  }
+}
+
+const format = (byteStream) => {
+  const part1 = Buffer.from(byteStream.slice(0, 4)).toString('hex')
+  const part2 = Buffer.from(byteStream.slice(4, 6)).toString('hex')
+  const part3 = Buffer.from(byteStream.slice(6, 8)).toString('hex')
+  const part4 = Buffer.from(byteStream.slice(8, 10)).toString('hex')
+  const part5 = Buffer.from(byteStream.slice(10, 16)).toString('hex')
   return `${part1}-${part2}-${part3}-${part4}-${part5}`
 }
 
-exports.generate = (interval, length) => {
-  return getSeqBytes(interval, length)
+const generate = (interval, length) => {
+  const byteStream = getSeq(interval, length)
+  fillRandom(byteStream)
+  return byteStream
 }
+
+module.exports = (interval, length) => {
+  return format(generate(interval, length))
+}
+
+module.exports.format = format
+module.exports.generate = generate
--- test/index.test.js
+++ test/index.test.js
@@ -1,6 +1,20 @@
 const test = require('tape')
 const sinon = require('sinon')
+const crypto = require('crypto')
 const { format, generate } = require('../')
+const sequuid = require('../')
+
+test('#sequuid', t => {
+  t.test('should return a formatted sequential uuid', q => {
+    sinon.stub(Date, 'now').returns(0xabcd)
+    sinon.stub(crypto, 'randomBytes').withArgs(16).returns(
+      Buffer.from([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16])
+    )
+    q.deepEqual(sequuid(1, 1), 'cd010203-0405-0607-0809-0a0b0c0d0e0f')
+    sinon.restore()
+    q.end()
+  })
+})
 
 test('#format', t => {
   t.test('should format from bytes to stardard uuid string', q => {
@@ -42,6 +56,17 @@ test('#generate', t => {
   t.test('should generate two seq bytes if interval is 60s and length is 2 bytes', q => {
     sinon.stub(Date, 'now').returns(0xabcd * 60 * 1000)
     q.deepEqual(generate(60 * 1000, 2).slice(0, 2), [0xab, 0xcd])
+    sinon.restore()
+    q.end()
+  })
+
+  t.test('should generate tail bytes randomly', q => {
+    sinon.stub(Date, 'now').returns(0xabcd)
+    sinon.stub(crypto, 'randomBytes').withArgs(16).returns(
+      Buffer.from([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16])
+    )
+    q.deepEqual(generate(1, 1).slice(1, 16),
+      [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])
     sinon.restore()
     q.end()
   })
